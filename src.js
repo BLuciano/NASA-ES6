91f473e332da7e36a5fd07a35de8e0975b0c5bdb
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2018-11-11 21:54:49 -0800

    Added styles and changed layout

--- src.js
+++ src.js
@@ -3,12 +3,14 @@
 let url = "https://images-api.nasa.gov/search?q=";
 let btn = document.getElementById('search-btn');
 let messages = document.getElementById("msg");
+let dataWrap = document.getElementById('results');
 
 //Calls getData on button click with user search or default value
 btn.addEventListener('click', (e) => {
   let query = document.getElementById("query").value;
   document.getElementById("query").value = "";
   btn.innerText = "searching...";
+  dataWrap.innerHTML = "";
   query === "" ? getData('stars') : getData(query);
 });
 
@@ -37,21 +39,22 @@ function displayData(JsonData){
   let data = JsonData.collection.items;
   let results = "";
 
-  if(data.length > 0){
-    data.forEach(x =>{
-      //Only displays the results with image type
-      if(x.data[0].media_type === "image"){
-          let {title, description : desc} = x.data[0];
-          let src = x.links[0].href;
-          results += "<article class='single-result'>";
-          results += `<img src='${src}'>`;
-          results += `<h3 class='title'>${title}</h3>`;
-          results += `<p class='desc'>${desc}</p></article>`;
-      }
-    });
-  } else {
+  data.forEach(x =>{
+    //Only displays the results with image type
+    if(x.data[0].media_type === "image"){
+      let {title, description : desc} = x.data[0];
+      let src = x.links[0].href;
+      results += "<article class='single-result'>";
+      results += `<img src="${src}">`;
+      results += `<h3 class='title'>${title}</h3></article>`;
+      //results += `<p class='desc'>${desc}</p></article>`;
+    }
+  });
+
+  if(results === ""){
     messages.innerText = "Sorry! The are no results for this search.";
+  } else {
+    dataWrap.innerHTML = results;
   }
-  document.getElementById('results').innerHTML = results;
   btn.innerText = "search";
 }
